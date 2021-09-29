4dff36fed824b512105725795558de166bd5634f
spencerporter <scfp419@gmail.com>
2021-09-28 19:22:13 -0600

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div className="app">
+            Hello World
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
