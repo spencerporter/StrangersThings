ce1b97ce7e2fd3cdc506983c4c74e96a01690499
spencerporter <scfp419@gmail.com>
2021-09-28 21:21:02 -0600

    Final Checkin of 9/28

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { BrowserRouter , Route , Link} from 'react-router-dom';
 const NavBar = (currentUser) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <div className="d-flex justify-content-between">
+            <div className="d-flex justify-content-between p-3">
                 <a className="navbar-brand fs-2" href="/">Stranger's Things</a>
 
                 <div className="collapse navbar-collapse fs-3" id="navbarNavAltMarkup">
@@ -12,7 +12,8 @@ const NavBar = (currentUser) => {
                         <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         <Link className="nav-link" to="/posts">Posts</Link>
                         <Link className="nav-link" to="/profile">Profile</Link>
-                        {(currentUser ? <Link className="nav-link" to="/login">Log In</Link> : <Link className="nav-link disabled">Log Out</Link>)}
+                        {(currentUser ? <Link className="nav-link" to="/login">Log In</Link> : 
+                        <Link className="nav-link disabled">Log Out</Link>)}
                     </div>
                 </div>
             </div>
