7b3c191470e926fb443103adf69fc2864c55e83e
spencerporter <scfp419@gmail.com>
2021-10-05 21:21:34 -0600

    pushing build

---
+++ build/precache-manifest.c85dbea5f543b011324c9385cf09d993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82382aec65b9b9e634d263306e011b2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68407f44a0bafdc45f80",
+    "url": "/static/js/2.42891f5c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.42891f5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0889cfb29dbcfd77941",
+    "url": "/static/js/main.e7b4f530.chunk.js"
+  },
+  {
+    "revision": "6fa31919e672cdd17db0",
+    "url": "/static/js/runtime-main.9df7520e.js"
+  }
+]);
